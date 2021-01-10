973b36686964f83dc317181ab850e7e435afa24c
Lilia <lilia.manukyan@hotmail.com>
2021-01-10 16:02:09 +0400

    user's post page

--- media-feed-app/src/App.js
+++ media-feed-app/src/App.js
@@ -11,6 +11,8 @@ import { AddPostForm } from "./features/posts/AddPostForm";
 import { EditPostForm } from "./features/posts/EditPostForm";
 import { PostList } from "./features/posts/PostList";
 import { SinglePostPage } from "./features/posts/SinglePostPage";
+import { UserList } from "./features/users/UserList";
+import { UserPage } from "./features/users/UserPage";
 
 function App() {
   return (
@@ -30,6 +32,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPosts/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UserList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
