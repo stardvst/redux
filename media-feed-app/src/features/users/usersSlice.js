1bbfeb40f83c4d0867cf9183ccf8e8093e33643c
Lilia <lilia.manukyan@hotmail.com>
2021-01-10 15:18:36 +0400

    fetching users

--- media-feed-app/src/features/users/usersSlice.js
+++ media-feed-app/src/features/users/usersSlice.js
@@ -1,15 +1,22 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { client } from "../../api/client";
 
-const initialState = [
-  { id: nanoid(), name: "Tianna Jenkins" },
-  { id: nanoid(), name: "Kevin Grant" },
-  { id: nanoid(), name: "Madison Price" },
-];
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await client.get("/fakeApi/users");
+  return response.users;
+});
+
+const initialState = [];
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchUsers.fulfilled]: (state, action) => {
+      return action.payload;
+    },
+  },
 });
 
 export default usersSlice.reducer;
--- media-feed-app/src/index.js
+++ media-feed-app/src/index.js
@@ -4,8 +4,12 @@ import "./index.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
 
 import "./api/server";
+
+// fetch users once, when app starts
+store.dispatch(fetchUsers());
 
 ReactDOM.render(
   <React.StrictMode>
