39e8a4cf8a4219091b7eb3d43c6ea176155bba30
Lilia <lilia.manukyan@hotmail.com>
2021-08-26 11:20:47 +0400

    redux tracing

--- redux-mosh-tutorial/src/index.js
+++ redux-mosh-tutorial/src/index.js
@@ -6,6 +6,8 @@ const unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch(bugAdded('Bug1'));
+store.dispatch(bugAdded('Bug2'));
+store.dispatch(bugAdded('Bug3'));
 // unsubscribe();
 store.dispatch(bugResolved(1));
 store.dispatch(bugRemoved(1));
