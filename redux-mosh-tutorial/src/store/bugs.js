ca9f4483172ab8c3a1a355865c3b5b7f2eb9ebf5
Lilia <lilia.manukyan@hotmail.com>
2021-08-27 10:16:32 +0400

    creating slices <3

--- redux-mosh-tutorial/src/store/bugs.js
+++ redux-mosh-tutorial/src/store/bugs.js
@@ -1,27 +1,28 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
-
-export const bugAdded = createAction('bugAdded');
-export const bugRemoved = createAction('bugRemoved');
-export const bugResolved = createAction('bugResolved');
-export const bugUpdated = createAction('bugUpdated');
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit';
 
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
 
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex(bug => bug.id === action.payload.id);
-    bugs[index].resolved = true;
-  },
-
-  [bugRemoved.type]: (bugs, action) => {
-    return bugs.filter(bug => bug.id !== action.payload.id);
+    bugRemoved: (bugs, action) => {
+      return bugs.filter(bug => bug.id !== action.payload.id);
+    },
   },
 });
+
+export default slice.reducer;
+export const { bugAdded, bugResolved, bugRemoved } = slice.actions;
