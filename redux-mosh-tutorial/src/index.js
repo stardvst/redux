6e3f86d253c19878ee6d2d77baee3aee8d533f9c
Lilia <lilia.manukyan@hotmail.com>
2021-08-27 23:06:35 +0400

    combining reducers

--- redux-mosh-tutorial/src/index.js
+++ redux-mosh-tutorial/src/index.js
@@ -1,11 +1,14 @@
 import configureAppStore from './store/configureStore';
 import { bugAdded, bugRemoved, bugResolved } from './store/bugs';
+import { projectAdded } from './store/projects';
 
 const store = configureAppStore();
 
 const unsubscribe = store.subscribe(() => {
   console.log('State changed!', store.getState());
 });
+
+store.dispatch(projectAdded({ name: 'Project1' }));
 
 store.dispatch(bugAdded({ description: 'Bug1' }));
 store.dispatch(bugAdded({ description: 'Bug2' }));
