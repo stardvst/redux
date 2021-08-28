cdd544a8396d4da115f59d36dfe5b7510c493e2b
Lilia <lilia.manukyan@hotmail.com>
2021-08-28 17:26:29 +0400

    simple middleware

--- redux-mosh-tutorial/src/index.js
+++ redux-mosh-tutorial/src/index.js
@@ -18,22 +18,22 @@ const unsubscribe = store.subscribe(() => {
 
 store.dispatch(projectAdded({ name: 'Project1' }));
 
-store.dispatch(userAdded({ name: 'User1' }));
-store.dispatch(userAdded({ name: 'User2' }));
+// store.dispatch(userAdded({ name: 'User1' }));
+// store.dispatch(userAdded({ name: 'User2' }));
 
-store.dispatch(bugAdded({ description: 'Bug1' }));
-store.dispatch(bugAdded({ description: 'Bug2' }));
-store.dispatch(bugAdded({ description: 'Bug3' }));
-// unsubscribe();
-store.dispatch(bugResolved({ id: 1 }));
-// store.dispatch(bugRemoved({ id: 1 }));
+// store.dispatch(bugAdded({ description: 'Bug1' }));
+// store.dispatch(bugAdded({ description: 'Bug2' }));
+// store.dispatch(bugAdded({ description: 'Bug3' }));
+// // unsubscribe();
+// store.dispatch(bugResolved({ id: 1 }));
+// // store.dispatch(bugRemoved({ id: 1 }));
 
-store.dispatch(bugAssigned({ bugId: 1, userId: 1 }));
-store.dispatch(bugAssigned({ bugId: 3, userId: 2 }));
+// store.dispatch(bugAssigned({ bugId: 1, userId: 1 }));
+// store.dispatch(bugAssigned({ bugId: 3, userId: 2 }));
 
-const x = getUnresolvedBugs(store.getState());
-const y = getUnresolvedBugs(store.getState());
-console.log(x === y);
+// const x = getUnresolvedBugs(store.getState());
+// const y = getUnresolvedBugs(store.getState());
+// console.log(x === y);
 
-const assignedBugs = getAssignedBugs(1)(store.getState(), 1);
-console.log(assignedBugs);
+// const assignedBugs = getAssignedBugs(1)(store.getState(), 1);
+// console.log(assignedBugs);
--- redux-mosh-tutorial/src/store/configureStore.js
+++ redux-mosh-tutorial/src/store/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from './middleware/logger';
 import reducer from './reducer';
 
 export default function configureAppStore() {
   return configureStore({
     reducer, // root reducer
+    middleware: [logger],
   });
 }
