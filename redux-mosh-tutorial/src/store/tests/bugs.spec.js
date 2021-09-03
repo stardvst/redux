7058222eae98f0571171535b133d361cb5f7d1b6
Lilia <lilia.manukyan@hotmail.com>
2021-09-03 20:58:03 +0400

    writing clean tests, aaa

--- redux-mosh-tutorial/src/store/tests/bugs.spec.js
+++ redux-mosh-tutorial/src/store/tests/bugs.spec.js
@@ -4,15 +4,37 @@ import { addBug } from '../bugs';
 import configureAppStore from './../configureStore';
 
 describe('bugsSlice', () => {
-  it('should handle addBug action', async () => {
+  let fakeAxios;
+  let store;
+
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios);
+    store = configureAppStore();
+  });
+
+  const bugsSlice = () => {
+    return store.getState().entities.bugs;
+  };
+
+  it("should add the bug to the store if it's saved to the server", async () => {
+    // arrange
     const bug = { description: 'a' };
     const savedBug = { ...bug, id: 1 };
-
-    const fakeAxios = new MockAdapter(axios);
     fakeAxios.onPost('/bugs').reply(200, savedBug);
 
-    const store = configureAppStore();
+    // act
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+
+    // assert
+    expect(bugsSlice().list).toContainEqual(savedBug);
+  });
+
+  it("should not add the bug to the store if it's not saved to the server", async () => {
+    const bug = { description: 'a' };
+    fakeAxios.onPost('/bugs').reply(500);
+
+    await store.dispatch(addBug(bug));
+
+    expect(bugsSlice().list).toHaveLength(0);
   });
 });
