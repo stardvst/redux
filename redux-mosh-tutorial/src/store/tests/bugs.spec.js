97194dd9a989f5131c1273e5b3666207f32fa1e4
Lilia <lilia.manukyan@hotmail.com>
2021-09-03 18:53:45 +0400

    mocking http calls

--- redux-mosh-tutorial/src/store/tests/bugs.spec.js
+++ redux-mosh-tutorial/src/store/tests/bugs.spec.js
@@ -1,10 +1,18 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { addBug } from '../bugs';
 import configureAppStore from './../configureStore';
 
 describe('bugsSlice', () => {
   it('should handle addBug action', async () => {
+    const bug = { description: 'a' };
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost('/bugs').reply(200, savedBug);
+
     const store = configureAppStore();
-    await store.dispatch(addBug());
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    await store.dispatch(addBug(bug));
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
